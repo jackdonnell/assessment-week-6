e7e5a17711112b8de62092f2aa1c805545b074b6
Jack <jackdonnell74@gmail.com>
2022-07-23 01:07:35 +0700

    2

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,15 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('clicking draw btn displays div with id = "choices"', async () => {
+    const drawBtn = await driver.findElement(By.id('draw'))
+    const div = await driver.findElement(By.id('choices'))
+    expect(drawBtn).toEqual(div)
+})
+
+test('Check that clicking an “DUEL!” button displays the div with id = “results”', async () => {
+    const duelBtn = await driver.findElement(By.id('duel'))
+    const div1 = await driver.findElement(By.id('results'))
+    expect(duelBtn).toEqual(div1)
+})
--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,12 @@
-const {shuffleArray} = require('./utils')
+const {shuffleArray, array, arrCopy} = require('./utils')
+
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('should be type string', () => {
+        expect(shuffleArray()).toEqual("array")
+        // expect(typeof shuffleArray.arrCopy).toBe('array')
+    })
+    test('array of same length', () => {
+        expect (arrCopy.length).toEqual(array.length)
+    })
 })
